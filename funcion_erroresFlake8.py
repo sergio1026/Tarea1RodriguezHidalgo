6c7935f3e37d24476d2a140f8893a2409f9a6bf1
SergioRodm <sergio.rmtata@gmail.com>
2020-09-13 20:12:44 -0600

    Flake8 Errors
    Signed-off-by: SergioRodm <sergio.rmtata@gmail.com>

--- funcion_erroresFlake8.py
+++ funcion_erroresFlake8.py
@@ -1,7 +1,15 @@
-def funcion_SumaAbsoluta(entrada_1, entrada_2):
 
-    if ((entrada_1) < (entrada_2)):
-        return entrada_2 - entrada_1
+# ERROR E231 = no dejar espacio despues de una coma
+# ERROR E203 = mucho espacio antes de los ´:´
+# ERROR E271 = mucho espacio despues de una palabra reservada como en los dos return que hay en el codigo prueba
+# ERROR E501 = una linea es muy larga, como la anterior a esta
+# ERROR E201 = espacio entre dos parentesis 
+
+def funcion_SumaAbsoluta(entrada_1,entrada_2) :
+
+    if ( ( entrada_1 ) < ( entrada_2 ) ): 
+        return  entrada_2 - entrada_1
 
     else:
-        return entrada_1 - entrada_2
+        return  entrada_1 - entrada_2
+
