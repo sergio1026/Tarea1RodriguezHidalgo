cf8e0927f298d0dabcc685da74c2e80dddefb2fa
SergioRodm <sergio.rmtata@gmail.com>
2020-09-25 14:06:28 -0600

    manejo de hilos 1.0

---
+++ Manejo de hilos.py
@@ -0,0 +1,17 @@
+import threading
+import time
+import math
+
+
+def raices():
+    lista = list(range(1, 100000, 1))
+    respuesta = []
+    for i in lista:
+        respuesta.append(math.sqrt(i))
+    return respuesta
+
+
+t0 = time.time()
+print(raices(), '\n')
+tf = time.time() - t0
+print('Tiempo de ejecución: ', tf)
